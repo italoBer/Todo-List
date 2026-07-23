a995550ef1e52b81a8da3b50f96cc739d858c1c9
ItaloBer <italoubernardo123@gmail.com>
2026-07-23 16:37:41 -0300

    Tasks table

---
+++ src/main/java/br/com/italoBer/todolist/task/TaskModel.java
@@ -0,0 +1,47 @@
+package br.com.italoBer.todolist.task;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+/**
+ *
+ * id
+ * usuario(id_usuario)
+ * descrição
+ * titulo
+ * data de inicio
+ * data de termino
+ * prioridade
+ *
+ */
+@Data
+@Entity(name = "tb_tasks")
+public class TaskModel {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+    private String description;
+
+    @Column(length = 50)
+    private String title;
+    private LocalDateTime startAt;
+    private LocalDateTime endtAt;
+    private String priority;
+
+    private UUID idUser;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+
+
+
+
+}
